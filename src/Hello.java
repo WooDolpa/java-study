f20675c6783ece3df4946a45e97bf1e94eea9c99
jwlee <dannis1124@naver.com>
2022-08-31 23:09:18 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * packageName : PACKAGE_NAME
+ * className : Hello
+ * user : jwlee
+ * date : 2022/08/31
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");    // 화면에 글자를 출력한다.
+    }
+}
