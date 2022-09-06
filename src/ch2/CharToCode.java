01e8624b06f17a6b4062e9465d04c1f6446baa5e
jwlee <dannis1124@naver.com>
2022-09-06 23:19:44 +0900

    자바의 정석 2장 공부중..!!

---
+++ src/ch2/CharToCode.java
@@ -0,0 +1,20 @@
+package ch2;
+
+/**
+ * packageName : ch2
+ * className : CharToCode
+ * user : jwlee
+ * date : 2022/09/06
+ */
+public class CharToCode {
+
+    public static void main(String[] args) {
+        char ch = 'A';          // char ch = 65;
+        int code = (int)ch;     // ch 에 저장된 값을 int 타입으로 변환하여 저장한다.
+
+        System.out.printf("%c=%d(%#X)%n", ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+    }
+}
