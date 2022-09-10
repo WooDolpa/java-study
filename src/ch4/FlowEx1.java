77038f769115e004e5a74430891c13d0a9146ca0
jwlee <dannis1124@naver.com>
2022-09-10 22:37:11 +0900

    4~5장 예제 코드 실습

---
+++ src/ch4/FlowEx1.java
@@ -0,0 +1,28 @@
+package ch4;
+
+/**
+ * packageName : ch4
+ * className : FlowEx1
+ * user : jwlee
+ * date : 2022/09/09
+ */
+public class FlowEx1 {
+
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x=%d 일때, 참인 것은%n", x);
+
+        if(x==0) System.out.println("x==0");
+        if(x != 0) System.out.println("x != 0");
+        if(!(x==0)) System.out.println("!(x==0)");
+        if(!(x != 0)) System.out.println("!(x!=0)");
+
+        x = 1;
+        System.out.printf("x+%d 일때, 참인 것은%n", x);
+
+        if(x==0) System.out.println("x==0");
+        if(x != 0) System.out.println("x!=0");
+        if(!(x==0)) System.out.println("!(x==0)");
+        if(!(x!=0)) System.out.println("!(x!=0)");
+    }
+}
