97b50f3854ac306e6d2c8bd4209c01e0c3a2ac8f
jwlee <dannis1124@naver.com>
2022-10-28 23:57:36 +0900

    Level 클래스 파일 추가

---
+++ src/fastcampus/chapter03/ch16/SuperLevel.java
@@ -0,0 +1,29 @@
+package fastcampus.chapter03.ch16;
+
+/**
+ * packageName : fastcampus.chapter03.ch16
+ * className : SuperLevel
+ * user : jwlee
+ * date : 2022/10/28
+ */
+public class SuperLevel implements PlayerLevel{
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void turn() {
+
+    }
+
+    @Override
+    public void showLevelMessage() {
+
+    }
+}
