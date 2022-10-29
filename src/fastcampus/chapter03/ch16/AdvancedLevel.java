eaf88d0087bc035ae2f7f8e60b53183428c3ac12
jwlee <dannis1124@naver.com>
2022-10-29 22:43:16 +0900

    update

--- src/fastcampus/chapter03/ch16/AdvancedLevel.java
+++ src/fastcampus/chapter03/ch16/AdvancedLevel.java
@@ -9,17 +9,17 @@ package fastcampus.chapter03.ch16;
 public class AdvancedLevel implements PlayerLevel{
     @Override
     public void run() {
-
+        System.out.println("천천히 달립니다.");
     }
 
     @Override
     public void jump() {
-
+        System.out.println("jump 못하지롱");
     }
 
     @Override
     public void turn() {
-
+        System.out.println("turn 못하지롱");
     }
 
     @Override
