0e1ace9ac41749a8a2899b641beacd9c38d22399
jwlee <dannis1124@naver.com>
2022-11-08 21:43:04 +0900

    인터페이스 정책 프로그래밍 작성진행

--- src/fastcampus/chapter03/ch16/scheduler/PriorityAllocation.java
+++ src/fastcampus/chapter03/ch16/scheduler/PriorityAllocation.java
@@ -10,11 +10,11 @@ public class PriorityAllocation implements Scheduler{
 
     @Override
     public void getNextCall() {
-
+        System.out.println("고객등급이 높은 고객의 call ");
     }
 
     @Override
     public void sendCallToAgent() {
-
+        System.out.println();
     }
 }
