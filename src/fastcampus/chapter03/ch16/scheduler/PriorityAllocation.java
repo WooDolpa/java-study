52b8530f458baf6938e9e5693a581e797be69dd6
jwlee <dannis1124@naver.com>
2022-11-09 12:13:53 +0900

    VIP 회원에 대한 안내원 구현

--- src/fastcampus/chapter03/ch16/scheduler/PriorityAllocation.java
+++ src/fastcampus/chapter03/ch16/scheduler/PriorityAllocation.java
@@ -10,11 +10,11 @@ public class PriorityAllocation implements Scheduler{
 
     @Override
     public void getNextCall() {
-        System.out.println("고객등급이 높은 고객의 call ");
+        System.out.println("고객등급이 높은 고객의 call을 먼저 가져갑니다.");
     }
 
     @Override
     public void sendCallToAgent() {
-        System.out.println();
+        System.out.println("업무 숙련도가 높은 상담원에게 먼저 배분합니다.");
     }
 }
